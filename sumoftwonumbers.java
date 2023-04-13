cfaadfab420263fd22590489dcf2c632e274059b
Raushan Raj <raj.raushan9101@gmail.com>
2023-04-13 19:44:04 +0530

    Update sumoftwonumbers.java

--- sumoftwonumbers.java
+++ sumoftwonumbers.java
@@ -3,10 +3,10 @@ public class sumoftwonumbers{
     public static void main(String[] args){
         Scanner input=new Scanner(System.in);
         System.out.print("Enter the value of 1st number: ");
-        int num1=input.nextInt();
+        int num1=input.nextInt();                               //taking first number from user
         System.out.print("Enter the value of 2nd number: ");
-        int num2=input.nextInt();
-        int sum=num1+num2;
-        System.out.println("The sum of 1st and 2nd number is: "+sum);
+        int num2=input.nextInt();                              //taking second numbr from user
+        int sum=num1+num2;                           
+        System.out.println("The sum of 1st and 2nd number is: "+sum);   //it will give the output of first number + second number
     }
 }
