a863730ed6ef87497e3df8738ddb526e14a89d32
Raushan Raj <raj.raushan9101@gmail.com>
2023-04-14 15:32:37 +0530

    String input in java

---
+++ baisc/stringinput.java
@@ -0,0 +1,9 @@
+import java.util.Scanner; // to import scanner class to take inputs
+
+public class stringinput{
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in); //scanner class to take from system
+        String name=input.nextLine(); //it will take a strings line given by user 
+        System.out.print(name);  //it will print the output
+    }
+}
