6c26a383d142a88e32f083d6654ec616c5b0769e
Raushan Raj <raj.raushan9101@gmail.com>
2023-04-19 10:50:15 +0530

    Febonacci number

---
+++ Loops/febonacci.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class febonacci {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int n=input.nextInt();
+        int p=0;         //previous number 
+        int i=1;        //Current index
+        int count=2;
+        
+        while(count<=n){
+            int temp=i;
+            i=i+p;
+            p=temp;
+            count++;
+        }
+        System.out.print(i);
+        
+    }
+}
