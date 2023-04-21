2357d24c349012d5cd2ee4822f40fe2e5e8652b3
Raushan Raj <raj.raushan9101@gmail.com>
2023-04-21 11:47:45 +0530

    Calculate Product
    calculate product of two numbers using function

---
+++ Functions/product.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Function{
+    public static int calculateProduct(int a,int b){
+        return a*b;   //directly returning the product
+    }
+    public static void main(String[] args){
+        Scanner in=new Scanner(System.in);
+        int a=in.nextInt();
+        int b=in.nextInt();
+        System.out.print(calculateProduct(a,b));
+    }
+}
