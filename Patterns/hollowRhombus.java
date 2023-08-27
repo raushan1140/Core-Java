f1dd1db39d1ccdbd1751048ed0733905c949f508
Raushan Raj <raj.raushan9101@gmail.com>
2023-08-28 03:07:10 +0530

    Hollow Rhombus

--- Patterns/hollowRhombus.java
+++ Patterns/hollowRhombus.java
@@ -1,6 +1,6 @@
 import java.util.*;
 import java.util.Scanner;
-public class hollowRohmbus {
+public class hollowRhombus {
     public static void main(String args[]) {
 	    Scanner sc = new Scanner(System.in);
         int n=sc.nextInt();
