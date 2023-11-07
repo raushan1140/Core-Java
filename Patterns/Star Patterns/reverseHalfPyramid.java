deb5844d2cea5a7757967b9fdbd3fda33503d562
Raushan Raj <raj.raushan9101@gmail.com>
2023-11-08 01:28:00 +0530

    Reverse Half Pyramid

---
+++ Patterns/Star Patterns/reverseHalfPyramid.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class reverseHalfPyramid{
+    public static void main(String[] args){
+        Scanner input=new Scanner(System.in);
+        int n=input.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<n-i;j++)
+            {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        
+    }
+}
