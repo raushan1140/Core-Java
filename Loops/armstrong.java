5bd1959fc700e2c3dd876b6c4dd639e8b963bde9
Raushan Raj <raj.raushan9101@gmail.com>
2023-12-14 14:49:35 +0530

    Armstrong or Not

--- Loops/armstrong.java
+++ Loops/armstrong.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 
 public class armstrong{
@@ -6,21 +5,30 @@ public class armstrong{
         Scanner input=new Scanner(System.in);
         System.out.print("Enter a number: ");
         int n=input.nextInt();
+        int num=n;
         int temp=n;
         int sum=0;
+        int count=0;
         while(n>0)
         {
-            int digit=n%10;
-            sum+=Math.pow(digit,3);
+            int ld=n%10;
+            count=count+1;
             n=n/10;
         }
-        if(sum==temp)
+        int ct=count;
+        while(num>0)
         {
-            System.out.println(n + " is an Armstrong number.");
+            int l=num%10;
+            sum+=Math.pow(l,ct);
+            num=num/10;
+        }
+        if(temp==sum)
+        {
+            System.out.print("Armstrong");
         }
         else
         {
-            System.out.println(n + " is not an Armstrong number.");
+            System.out.print("Not Armstrong");
         }
     }
 }
